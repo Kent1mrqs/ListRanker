4df14a36446124d83eec453b1404b1264a7ea37b
Quentin <marquesquentin@icloud.com>
2024-10-10 19:55:45 +0300

    update list frontend

--- frontend/src/components/ListCreation.jsx
+++ frontend/src/components/ListCreation.jsx
@@ -1,36 +1,49 @@
-import {Button, Stack, TextField, Typography} from "@mui/material";
+import {Button, MenuItem, Select, Stack, TextField, Typography} from "@mui/material";
 import {useState} from "react";
 
 
 export default function ListCreation() {
+	const default_list = {
+		name: '',
+		elements:[]
+	}
+	const [nameList, setNameList] = useState('');
 	const [input, setInput] = useState('');
-	const [list, setList] = useState([]);
+	const [list, setList] = useState(default_list);
 	const [separator, setSeparator] = useState('\n');
 
 	function onClick() {
-		setList(input.split(separator));
-	}
-	function onChange(e) {
-		setInput(e.target.value);
+		setList({name: nameList, elements: input.split(separator)});
+		setInput('')
 	}
 
 	return (
-		<Stack spacing={1}>
-			<Typography>Nom de la liste</Typography>
-			<TextField></TextField>
-			<TextField
-				id="outlined-multiline-flexible"
-				label="Multiline"
-				multiline
-				onChange={onChange}
-				value={input}
-				maxRows={4}
-			/>
-			<Button onClick={onClick}>Validate</Button>
-			<Typography>Elements de la liste : </Typography>
-			{list.map(el => {
-				return (<Typography>{el}</Typography>)
-			})}
+		<Stack direction='row' spacing={3}>
+			<Stack spacing={1}>
+				<Typography>Nom de la liste</Typography>
+				<TextField onChange={e=> setNameList(e.target.value)}/>
+				<TextField
+					id="outlined-multiline-flexible"
+					label="Multiline"
+					multiline
+					onChange={e=>setInput(e.target.value) }
+					value={input}
+					maxRows={4}
+				/>
+				<Select value={separator} onChange={event => setSeparator(event.target.value)}>
+					<MenuItem value={'\n'}>Saut de ligne</MenuItem>
+					<MenuItem value=','>,</MenuItem>
+					<MenuItem value=';'>;</MenuItem>
+					<MenuItem value=' '>espace</MenuItem>
+				</Select>
+				<Button onClick={onClick}>Validate</Button>
+			</Stack>
+			<Stack spacing={1}>
+				<Typography>Elements de la liste {list.name}: </Typography>
+				{list.elements.map(el => {
+					return (<Typography>{el}</Typography>)
+				})}
+			</Stack>
 		</Stack>
 	)
 }
