167be42d68ac1f99c1beddd588fc2a160a498f5c
Quentin <marquesquentin@icloud.com>
2024-10-10 20:09:43 +0300

    http fetch front-back

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
+/*
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
@@ -22,7 +23,32 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
+} */
+
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
+
+        App::new()
+            .wrap(cors)
+            .route("/", web::get().to(hello))
+    })
+    .bind("127.0.0.1:8081")?
     .run()
     .await
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 import ListCreation from "./components/ListCreation";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8081/')
+            .then(response => response.text())
+            .then(data => setData(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
+console.log(data)
+
+    return (
     <div className="App">
       <header className="App-header">
           <ListCreation/>
