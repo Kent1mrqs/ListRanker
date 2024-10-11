71517b5247b92fe7cc77be52b7541a55ead00a99
Quentin <marquesquentin@icloud.com>
2024-10-11 22:52:36 +0300

    front update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,26 @@
 import './App.css';
 import ListCreation from "./components/ListCreation";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
+import {fetchData} from "./components/services/api";
 
 function App() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true); // État pour gérer le chargement
-    const [error, setError] = useState(null); // État pour gérer les erreurs
-    console.log(data ?? error)
+    const [users, setUsers] = useState([]);
+    const [lists, setLists] = useState([]);
+    console.log(users)
+    console.log(lists)
+
+    const fetchUsers = useCallback(() => {
+        fetchData('users', setUsers);
+    }, []);
+
+    const fetchLists = useCallback(() => {
+        fetchData('lists', setLists);
+    }, []);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:8080/users'); // Corrigez l'URL ici
-                console.log(response)
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des utilisateurs');
-                }
-                const result = await response.json();
-                setData(result);
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
+        fetchUsers()
+        fetchLists()
+    }, [fetchLists, fetchUsers ]);
 
     return (
         <div className="App">
