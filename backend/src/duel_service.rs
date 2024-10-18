7ed90c165a72d1bfbad3efd02af4db990bb33226
Quentin <marquesquentin@icloud.com>
2024-10-18 13:29:46 +0300

    [backend] factorize algo

--- backend/src/duel_service.rs
+++ backend/src/duel_service.rs
@@ -18,28 +18,34 @@ pub fn next_duel(conn: &mut PgConnection, ranking_id: i32, winner: Winner) -> Re
     Ok(()) // Ensure it returns `Result`
 }
 
+fn pick_random_dual_candidates(max: usize) -> (usize, usize) {
+    let mut rng = rand::thread_rng();
+    let mut position_1 = rng.gen_range(0..max);
+    let mut position_2 = rng.gen_range(0..max);
+    while position_1 == position_2 {
+        position_2 = rng.gen_range(0..max);
+    }
+    (position_1, position_2)
+}
+
 
 /// Je reçois l'id du ranking, je crée ou load une session?
 pub fn init_duel(conn: &mut PgConnection, ranking_id: i32) -> Result<Vec<ItemDuel>, Box<dyn std::error::Error>> {
-    let response = random_duel(conn, ranking_id)?;
+    let response = pick_duel_candidates(conn, ranking_id, pick_random_dual_candidates)?;
     Ok(response)
 }
 
+
 /// Retrieves two different random items from a ranking list based on the given ranking ID.
-pub fn random_duel(conn: &mut PgConnection, ranking_id: i32) -> Result<Vec<ItemDuel>, Box<dyn std::error::Error>> {
+pub fn pick_duel_candidates(conn: &mut PgConnection, ranking_id: i32, algo: fn(usize) -> (usize, usize)) -> Result<Vec<ItemDuel>, Box<dyn std::error::Error>> {
     let items = get_ranking_items_by_ranking_id(conn, ranking_id)?;
     let list_size = items.len();
     if list_size < 2 {
         return Err("Not enough items for a duel.".into());
     }
 
-    let mut rng = rand::thread_rng();
-    let mut position_1 = rng.gen_range(0..list_size);
-    let mut position_2 = rng.gen_range(0..list_size);
+    let (position_1, position_2) = algo(list_size);
 
-    while position_1 == position_2 {
-        position_2 = rng.gen_range(0..list_size);
-    }
 
     let item_1 = ItemDuel {
         id: position_1 as i32,
@@ -54,10 +60,14 @@ pub fn random_duel(conn: &mut PgConnection, ranking_id: i32) -> Result<Vec<ItemD
 
     Ok(vec![item_1, item_2])
 }
+
+
+fn best_choice(max: usize) -> (usize, usize) {
+    (1, 1)
+}
+
+
 pub fn store_winner(conn: &mut PgConnection, id_session: i32) -> () {}
 
-pub fn create_session(conn: &mut PgConnection, id_session: i32) -> () {}
-
-pub fn load_session(conn: &mut PgConnection, id_session: i32) -> () {}
 
 pub fn generate_final_ranking(conn: &mut PgConnection, id_session: i32) -> () {}
