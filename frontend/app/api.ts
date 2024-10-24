e6afdc2cf6b60efbeb0c6a32789e9e2051ac16c4
Quentin <marquesquentin@icloud.com>
2024-10-24 13:56:48 +0300

    improve api.ts

--- frontend/app/api.ts
+++ frontend/app/api.ts
@@ -5,19 +5,18 @@ const handleAuth = (): string | null => {
             localStorage.removeItem("userId");
             window.location.href = '/signin';
         }
-        return null; // Retourne null si pas de token
-    } else {
-        const payload = JSON.parse(atob(token.split('.')[1]));
-        const exp = payload.exp * 1000; // Convertir en millisecondes
-        if (Date.now() >= exp && window.location.pathname !== '/signin' && window.location.pathname !== '/signup') {
-            localStorage.removeItem("userId");
-            window.location.href = '/signin';
-        }
+        return null;
     }
-    return token; // Retourne le token valide
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const exp = payload.exp * 1000; // Convertir en millisecondes
+    if (Date.now() >= exp && window.location.pathname !== '/signin' && window.location.pathname !== '/signup') {
+        localStorage.removeItem("userId");
+        window.location.href = '/signin';
+    }
+    return token;
 };
 
-const createFetchOptions = (method: string, token: string | null, data?: any) => {
+const createFetchOptions = (method: string, token: string | null, data?: any): RequestInit => {
     const options: RequestInit = {
         method,
         headers: {
@@ -31,12 +30,12 @@ const createFetchOptions = (method: string, token: string | null, data?: any) =>
     return options;
 };
 
-export const fetchData = async <R>(route: string): Promise<R> => {
-    const url = "http://127.0.0.1:8080/" + route;
+const apiRequest = async <T, R>(method: string, route: string, data?: T): Promise<R | undefined> => {
+    const url = `http://127.0.0.1:8080/${route}`;
     const token = handleAuth();
 
     try {
-        const response = await fetch(url, createFetchOptions('GET', token));
+        const response = await fetch(url, createFetchOptions(method, token, data));
         if (!response.ok) {
             throw new Error(String(response.status));
         }
@@ -49,56 +48,18 @@ export const fetchData = async <R>(route: string): Promise<R> => {
     }
 };
 
-export const postData = async <T, R>(route: string, data: T): Promise<R> => {
-    const url = "http://127.0.0.1:8080/" + route;
-    const token = handleAuth();
- 
-    try {
-        const response = await fetch(url, createFetchOptions('POST', token, data));
-        if (!response.ok) {
-            throw new Error(String(response.status));
-        }
-        return await response.json() as R;
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error(error.message);
-        }
-        throw new Error('An unknown error occurred');
-    }
+export const fetchData = async <R>(route: string): Promise<R | undefined> => {
+    return apiRequest<undefined, R>('GET', route);
 };
 
-export const deleteData = async <T>(route: string): Promise<T> => {
-    const url = "http://127.0.0.1:8080/" + route;
-    const token = handleAuth();
-
-    try {
-        const response = await fetch(url, createFetchOptions('DELETE', token));
-        if (!response.ok) {
-            throw new Error(String(response.status));
-        }
-        return await response.json() as T;
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error(error.message);
-        }
-        throw new Error('An unknown error occurred');
-    }
+export const postData = async <T, R>(route: string, data: T): Promise<R | undefined> => {
+    return apiRequest<T, R>('POST', route, data);
 };
 
-export const editData = async <T>(route: string, data: T): Promise<T> => {
-    const url = "http://127.0.0.1:8080/" + route;
-    const token = handleAuth();
+export const deleteData = async <R>(route: string): Promise<R | undefined> => {
+    return apiRequest<undefined, R>('DELETE', route);
+};
 
-    try {
-        const response = await fetch(url, createFetchOptions('PUT', token, data));
-        if (!response.ok) {
-            throw new Error(String(response.status));
-        }
-        return await response.json() as T;
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error(error.message);
-        }
-        throw new Error('An unknown error occurred');
-    }
+export const editData = async <T, R>(route: string, data: T): Promise<R | undefined> => {
+    return apiRequest<T, R>('PUT', route, data);
 };
