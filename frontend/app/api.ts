6441230177b18f352b7c15eabace0932548035a6
Quentin <marquesquentin@icloud.com>
2024-10-24 13:52:52 +0300

    logout when token is missing or expired

--- frontend/app/api.ts
+++ frontend/app/api.ts
@@ -1,102 +1,104 @@
+const handleAuth = (): string | null => {
+    const token = localStorage.getItem('jwt');
+    if (!token) {
+        if (window.location.pathname !== '/signin' && window.location.pathname !== '/signup') {
+            localStorage.removeItem("userId");
+            window.location.href = '/signin';
+        }
+        return null; // Retourne null si pas de token
+    } else {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        const exp = payload.exp * 1000; // Convertir en millisecondes
+        if (Date.now() >= exp && window.location.pathname !== '/signin' && window.location.pathname !== '/signup') {
+            localStorage.removeItem("userId");
+            window.location.href = '/signin';
+        }
+    }
+    return token; // Retourne le token valide
+};
+
+const createFetchOptions = (method: string, token: string | null, data?: any) => {
+    const options: RequestInit = {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token ? `Bearer ${token}` : ''
+        },
+    };
+    if (data) {
+        options.body = JSON.stringify(data);
+    }
+    return options;
+};
+
 export const fetchData = async <R>(route: string): Promise<R> => {
     const url = "http://127.0.0.1:8080/" + route;
-    const token = localStorage.getItem('jwt');
-    console.info("fetch ", url)
+    const token = handleAuth();
+
     try {
-        const response = await fetch(url, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
+        const response = await fetch(url, createFetchOptions('GET', token));
         if (!response.ok) {
             throw new Error(String(response.status));
         }
-        const result = await response.json() as R;
-        return result;
+        return await response.json() as R;
     } catch (error) {
         if (error instanceof Error) {
             throw new Error(error.message);
-        } else {
-            throw new Error('An unknown error occurred');
         }
+        throw new Error('An unknown error occurred');
     }
 };
 
 export const postData = async <T, R>(route: string, data: T): Promise<R> => {
     const url = "http://127.0.0.1:8080/" + route;
-    const token = localStorage.getItem('jwt');
-    console.info("post ", url, " data : ", data)
+    const token = handleAuth();
+ 
     try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(data),
-        });
+        const response = await fetch(url, createFetchOptions('POST', token, data));
         if (!response.ok) {
             throw new Error(String(response.status));
         }
-        const result = await response.json() as R;
-        console.info("post response : ", response)
-        return result;
+        return await response.json() as R;
     } catch (error) {
         if (error instanceof Error) {
             throw new Error(error.message);
-        } else {
-            throw new Error('An unknown error occurred');
         }
+        throw new Error('An unknown error occurred');
     }
 };
 
 export const deleteData = async <T>(route: string): Promise<T> => {
     const url = "http://127.0.0.1:8080/" + route;
-    const token = localStorage.getItem('jwt');
+    const token = handleAuth();
+
     try {
-        const response = await fetch(url, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        });
+        const response = await fetch(url, createFetchOptions('DELETE', token));
         if (!response.ok) {
             throw new Error(String(response.status));
         }
-        const result = await response.json() as T;
-        console.info("post response : ", response)
-        return result;
+        return await response.json() as T;
     } catch (error) {
         if (error instanceof Error) {
             throw new Error(error.message);
-        } else {
-            throw new Error('An unknown error occurred');
         }
+        throw new Error('An unknown error occurred');
     }
 };
+
 export const editData = async <T>(route: string, data: T): Promise<T> => {
     const url = "http://127.0.0.1:8080/" + route;
-    const token = localStorage.getItem('jwt');
+    const token = handleAuth();
+
     try {
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(data),
-        });
+        const response = await fetch(url, createFetchOptions('PUT', token, data));
         if (!response.ok) {
             throw new Error(String(response.status));
         }
-        const result = await response.json() as T;
-        return result;
+        return await response.json() as T;
     } catch (error) {
         if (error instanceof Error) {
             throw new Error(error.message);
-        } else {
-            throw new Error('An unknown error occurred');
         }
+        throw new Error('An unknown error occurred');
     }
 };
